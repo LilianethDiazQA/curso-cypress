08c42f58bd3daf1720df1c23d8b6b876908ec5b8
Lilianeth Diaz QA <lilianethqa@gmail.com>
2024-07-18 16:57:44 -0400

    Configuracion de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require('cypress');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+const { configureAllureAdapterPlugins }  = require('@mmisty/cypress-allure-adapter/plugins') ;
 
 module.exports = defineConfig({
     e2e: {
+        "video": true,
         setupNodeEvents(on, config) {
-            allureWriter(on, config);
+            configureAllureAdapterPlugins(on, config);
             return config;
         }
     }
+    
 });
 
 
