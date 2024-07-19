b75444c10736c47f82907ebb3596f316241df81a
Lilianeth Diaz QA <lilianethqa@gmail.com>
2024-07-19 17:40:38 -0400

    Fix report and flakyness

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 const { configureAllureAdapterPlugins }  = require('@mmisty/cypress-allure-adapter/plugins') ;
 
 module.exports = defineConfig({
+    retries: 1,
     e2e: {
         "video": true,
         setupNodeEvents(on, config) {
--- cypress/e2e/ejemplo..cy.js
+++ cypress/e2e/ejemplo..cy.js
@@ -1,9 +1,17 @@
 // cypress/e2e/example.spec.js
 
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('Visitar sitio web y verificar contenido', () => {
     it('Visita el sitio web y verifica el contenido', () => {
       // Visita el sitio web
-      cy.visit('https://www.google.com/')
+      const usuario ='random01';
+      const contrasena = 'random01'
+      cy.visit('https://www.demoblaze.com/')
+      cy.get('a[data-target="#logInModal"]').click()
+      LoginMethods.login(usuario,contrasena)
+      cy.wait(5000)
+      cy.get('a#nameofuser').should('contain.text',usuario)
   
       // Verifica que el título de la página contenga 'Example Domain'
       //cy.contains('Example Domain').should('exist')
