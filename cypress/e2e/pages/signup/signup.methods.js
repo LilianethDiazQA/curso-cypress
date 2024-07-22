a2fb77aa746e696ccada96f03393166f1d3a1cff
Lilianeth Diaz QA <lilianethqa@gmail.com>
2024-07-22 16:32:05 -0400

    Creacion de pagina de signup

---
+++ cypress/e2e/pages/signup/signup.methods.js
@@ -0,0 +1,22 @@
+import { SignupElements } from "./signup.element";
+
+export class SignupMethods{
+    static inserUsername(username){
+        SignupElements.textboxes.username.invoke("val",username)
+    }
+
+    static insertPassword(password){
+    SignupElements.textboxes.password.invoke('val',password)
+}
+
+    static clickOnSignupButton(){
+        SignupElements.buttons.signup.click();
+    }
+
+    static signup (username,password){
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnSignupButton();
+
+    }
+}
