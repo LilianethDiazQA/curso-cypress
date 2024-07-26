ab04d9ab07f8b811e377ba7faea61b48930a0464
Lilianeth Diaz QA <lilianethqa@gmail.com>
2024-07-26 08:20:26 -0400

    Crecion de la pagina placeorder

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,13 @@
+export class PlaceOrderData{
+    static get testDate(){
+        return{
+            name:'Test name',
+            country: "United States",
+            city:"Boston",
+            creditCardNumber:"4012888888881881",
+            month:12,
+            year:2024,
+
+        }
+    }
+}
---
+++ cypress/e2e/pages/place-order/place-order.methods.js
@@ -0,0 +1,42 @@
+import { PlaceOrderElements } from "./place-order.elements";
+
+export class PlaceOrdermethods {
+    static insertName(name){
+        PlaceOrderElements.textboxes.name.invoke("val",name);
+    }
+
+    static insertCountry(country){
+        PlaceOrderElements.textboxes.country.invoke("val",country);
+    }
+ 
+    static inserCity(city){
+        PlaceOrderElements.textboxes.city.invoke("val", city);
+    }
+
+    static insertCreditCard(creditCardNumbrer){
+        PlaceOrderElements.textboxes.creditCard.invoke("val",creditCardNumbrer);
+    }
+
+    static insertMonth(month){
+        PlaceOrderElements.textboxes.month.invoke("val",month);
+    
+    }
+
+    static insertYear(year){
+        PlaceOrderElements.textboxes.year.invoke("val",year);
+        
+    }
+
+    static ClickOnCloseButton(){
+        PlaceOrderElements.buttons.close.click();
+    }
+
+    static ClickOnPurchaseButton(){
+
+        PlaceOrderElements.buttons.purchase.click();
+    }
+
+
+
+
+}
